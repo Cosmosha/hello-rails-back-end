5ec3fd9d470f216c49c707308ce4de9b5d6f154b
cosmosha <cosmoshagan@gmail.com>
2023-09-30 14:51:41 +0000

    Add migration, schema and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create(greeting: "Hello, world!")
+Message.create(greeting: "Hiya!")
+Message.create(greeting: "Welcome!")
+Message.create(greeting: "Hi, there!")
+Message.create(greeting: "Hey!")
+Message.create(greeting: "Greetings, friend!")
+
